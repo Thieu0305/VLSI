467a3e0fbf469c3b893df899d984589e1a07bca9
dgt030503 <21522631@gm.uit.edu.vn>
2025-01-23 17:51:38 +0700

    test

---
+++ generated-src/chipyard.harness.TestHarness.TinyRocketConfig/chipyard.harness.TestHarness.TinyRocketConfig.bb.f
@@ -0,0 +1,15 @@
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/ClockSourceAtFreqMHz.v
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/EICG_wrapper.v
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/GenericDigitalInIOCell.v
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/GenericDigitalOutIOCell.v
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/SimJTAG.cc
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/SimJTAG.v
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/SimTSI.cc
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/SimTSI.v
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/SimUART.cc
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/SimUART.v
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/plusarg_reader.v
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/remote_bitbang.cc
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/testchip_htif.cc
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/testchip_tsi.cc
+/home/acer/Documents/Chipyard/chipyard/vlsi/generated-src/chipyard.harness.TestHarness.TinyRocketConfig/gen-collateral/uart.cc
